cc9338276bb754b8d3ec2ef511c14cb44c8fdc7f
Daniel Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2024-02-21 22:49:49 +0000

    Added static root
    For static collection

--- DjangoDeployment/settings.py
+++ DjangoDeployment/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 
+STATIC_ROOT = 'staticroot'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
